4df185185e58c642dd76a3eb1a823e43dc8880fa
OTHMANI Rahma <rahma.othmani@soprahr.com>
2025-02-13 14:35:02 +0100

    front signup and login

--- front/formation/src/app/app.module.ts
+++ front/formation/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
